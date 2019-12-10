1d377d788c9753694abab84712d49f38a5ffffa4
Hieu Tran <hieu@espxmedia.com>
2019-12-10 11:56:34 +0800

    Change logic

--- scripts/buildnpm.sh
+++ scripts/buildnpm.sh
@@ -5,6 +5,6 @@ do
   rm -rf $component
   mkdir $component
   npx babel ./packages/$component/src/*.component.js -d ./$component --copy-files
-  cp -r ./packages/$component/css ./$component
+  cp -r ./packages/$component/src/css ./$component
   mv ./$component/*.js ./$component/index.js
 done
